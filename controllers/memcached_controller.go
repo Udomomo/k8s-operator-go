2967965884b073f958c33be629050602cbeff669
Udomomo <udomomo@gmail.com>
2021-10-23 16:39:32 +0900

    add logic when reconciled

--- controllers/memcached_controller.go
+++ controllers/memcached_controller.go
@@ -47,9 +47,8 @@ type MemcachedReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
 func (r *MemcachedReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
-
-	// your logic here
+	log := log.FromContext(ctx)
+	log.Info("Reconcile is called")
 
 	return ctrl.Result{}, nil
 }
